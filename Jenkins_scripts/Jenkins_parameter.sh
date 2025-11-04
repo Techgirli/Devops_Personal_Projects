7c845968107a0c15bd68333c3088f8279dff8170
techgirli <olorunsolamiracle@gmail.com>
2025-11-04 09:37:43 -0500

    This is my first parameter Jenkins

---
+++ Jenkins_scripts/Jenkins_parameter.sh
@@ -0,0 +1,7 @@
+!/bin/bash                                                                     
+
+FIRST_NAME=$1
+LAST_NAME=$2
+SKILL=$3
+
+echo "My full name is $FIRST_NAME $LAST_NAME  and I am a $SKILL Engineer"
