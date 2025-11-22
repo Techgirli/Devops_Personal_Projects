586b3c3e1e05fc659bbffec96c86bd83e7902ec0
Miracle  Olorunsola <142849935+Techgirli@users.noreply.github.com>
2025-11-22 16:44:08 -0500

    Revert "Mini Nodejs, Javascript  Python projects "

--- Python/__pycache__/Pythonnn.py
+++
@@ -1,6 +0,0 @@
-from Student import Student
-
-student1 = Student("Jim", "Business", 3.1, False)
-student2 = Student("Pam", "Art", 4.2, True)
-
-print(student2.on_honour_roll())
--- Python/__pycache__/app.py
+++
@@ -1,3 +0,0 @@
-friends = ["Jim", "Karen", "Kevin"]
-for index in range(3, 10):
-    print(index)
